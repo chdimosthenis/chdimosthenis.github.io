fe4a963fdc1796ec5724cfa453dd2587fa5d9ec1
chdimosthenis <dimos.chatzinikolaou@gmail.com>
2026-08-01 10:42:17 +0000

    Deploying to gh-pages from @ chdimosthenis/chdimosthenis.github.io@7441f3c2006d086f7050c70463dd19e29541fb92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "Peer-reviewed journal articles, chapters in edited volumes, and selected conference proceedings. ABS / ABDC / Scopus / WoS rankings shown where applicable. Last updated December 2025.",
+          description: "Peer-reviewed journal articles, chapters in edited volumes, and selected conference proceedings. AJG / ABDC / Scopus / WoS rankings shown where applicable. Last updated December 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
